787d646138a1da37553b5471907c2c4155612155
rune-encoder <132966351+rune-encoder@users.noreply.github.com>
2023-10-23 16:29:14 -0500

    Adjusted Media Queries, Set up WireFrame for About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,3 +1,26 @@
+import profilePhoto from "../assets/profile-photo.jpg";
+import "../styles/About.css";
+
+import aboutContent from "../content/aboutContent.js";
+
 export default function About() {
-  return <h1>About.JSX</h1>;
+  const { title, paragraphs, slogan } = aboutContent;
+
+  return (
+    <div className="container grid gap-2 laptop:grid-cols-3 large-mobile:grid-cols-2 mobile:grid-cols-1">
+      <div className="image-group laptop:w-auto">
+        <img className="profile-photo" src={profilePhoto} alt="Profile" />
+      </div>
+      <div className="about-group laptop:col-span-2">
+        <h3 className="title">{title}</h3>
+        {paragraphs.map((paragraph) => (
+          <p className="about-info" key={paragraph.id}>
+            {paragraph.text}
+          </p>
+        ))}
+        <br />
+        <h3 className="slogan">{slogan}</h3>
+      </div>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,18 +5,23 @@ export default {
   theme: {
     darkMode: "class",
     screens: {
-      mobile: "640px",
-      // => @media (min-width: 640px) { ... }
-      tablet: "768px",
-      // => @media (min-width: 768px) { ... }
-      desktop: "1024px",
-      // => @media (min-width: 1024px) { ... }
-      "large-desktop": "1280px",
-      // => @media (min-width: 1280px) { ... }
-      "extra-large-desktop": "1536px",
-      // => @media (min-width: 1536px) { ... }
+      "mobile": "576px",
+      "large-mobile": "768px",
+      "tablet": "992px",
+      "laptop": "1024px",
+      "desktop": "1280px",
+      "large-desktop": "1536px",
     },
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        '1': 'repeat(1, minmax(0, 1fr))',
+        '2': 'repeat(2, minmax(0, 1fr))',
+        '3': 'repeat(3, minmax(0, 1fr))',
+        '4': 'repeat(4, minmax(0, 1fr))',
+        '5': 'repeat(5, minmax(0, 1fr))',
+        '6': 'repeat(6, minmax(0, 1fr))',
+      },
+    },
   },
   plugins: [],
 };
