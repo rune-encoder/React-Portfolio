34ea9c337f85f2925feb36f4e546933bc998d7e1
rune-encoder <132966351+rune-encoder@users.noreply.github.com>
2023-10-24 14:31:31 -0500

    Created Style for Footer, Buttons, Links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,70 @@
-import { Link } from "react-router-dom";
+import "../styles/Footer.css";
+import {
+  FaGithub,
+  FaLinkedin,
+  FaTwitterSquare,
+  FaInstagram,
+  FaSpotify,
+} from "react-icons/fa";
+import { SiIndeed } from "react-icons/si";
 
 export default function Footer(props) {
-        
-    return (
-        <footer>
-        <h1>Footer.JSX</h1>
-        {props.children}
-        <a href="#" target="_blank" title="Visit Christian's Github">Github</a>
-        <a href="#" target="_blank" title="Visit Christian's LinkedIn">LinkedIn</a>
-        <a href="#" target="_blank" title="Visit Christian's Indeed">Indeed</a>
-        <a href="#" target="_blank" title="Visit Christian's Twitter">Twitter</a>
-        <a href="#" target="_blank" title="Visit Christian's Instagram">Instagram</a>
-        <a href="#" target="_blank" title="Visit Christian's Spotify">Spotify</a>
-        <h5>Website by Christian Palacios 2023</h5>
-        </footer>
-    );
-    }
+  return (
+    <footer>
+      {props.children}
+      <div className="footer-links-group">
+        <a
+          href="https://github.com/rune-encoder"
+          target="_blank"
+          title="Visit Christian's Github"
+          data-descr="GitHub"
+        >
+          <FaGithub className="icon" />
+        </a>
+        <a
+          href="www.linkedin.com/in/
+          christian-palacios-3536281aa
+          "
+          target="_blank"
+          title="Visit Christian's LinkedIn"
+          data-descr="LinkedIn"
+        >
+          <FaLinkedin className="icon" />
+        </a>
+        <a
+          href="https://profile.indeed.com/p/christianp-j1b1fb2"
+          target="_blank"
+          title="Visit Christian's Indeed"
+          data-descr="Indeed"
+        >
+          <SiIndeed className="icon" />
+        </a>
+        <a
+          href="https://twitter.com/rune_encoder"
+          target="_blank"
+          title="Visit Christian's Twitter"
+          data-descr="Twitter"
+        >
+          <FaTwitterSquare className="icon" />
+        </a>
+        <a
+          href="https://www.instagram.com/rune_encoder/"
+          target="_blank"
+          title="Visit Christian's Instagram"
+          data-descr="Instagram"
+        >
+          <FaInstagram className="icon" />
+        </a>
+        <a
+          href="https://open.spotify.com/user/1238388654?si=w9VCJZNHSyO980eQ_OVUEw"
+          target="_blank"
+          title="Visit Christian's Spotify"
+          data-descr="Spotify"
+        >
+          <FaSpotify className="icon" />
+        </a>
+      </div>
+      <h5 className="footer-text">Website by Christian Palacios</h5>
+    </footer>
+  );
+}
