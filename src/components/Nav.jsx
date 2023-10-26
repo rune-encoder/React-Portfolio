20424b952a9817e9c3ea05730785fbc45581e60c
rune-encoder <132966351+rune-encoder@users.noreply.github.com>
2023-10-26 08:36:20 -0500

    Updated Nav to display disabled when path is clicked on.

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,25 +1,36 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import Navbar from "./UI/Navbar.jsx";
 
 export default function Nav() {
+  const location = useLocation();
+  const links = [
+    { id: 1, path: "/About", text: "About" },
+    { id: 2, path: "/Portfolio", text: "Portfolio" },
+    { id: 3, path: "/Contact", text: "Contact" },
+    { id: 4, path: "/Resume", text: "Resume" },
+  ];
+
+  const isLinkDisabled = (link) => {
+    if (location.pathname === "/" && link.path === "/About") {
+      return true;
+    }
+
+    return link.path === location.pathname;
+  };
+
   return (
     <div className="nav-group">
       <nav>
         <Navbar
-          links={[
-            <Link key={1} to="/About">
-              About
-            </Link>,
-            <Link key={2} to="/Portfolio">
-              Portfolio
-            </Link>,
-            <Link key={3} to="/Contact">
-              Contact
-            </Link>,
-            <Link key={4} to="/Resume">
-              Resume
-            </Link>,
-          ]}
+          links={links.map((link) => (
+            <Link
+              key={link.id}
+              to={link.path}
+              className={isLinkDisabled(link) ? "disabled" : ""}
+            >
+              {link.text}
+            </Link>
+          ))}
         />
       </nav>
     </div>
