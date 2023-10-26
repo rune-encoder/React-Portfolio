a602c0b9aec8afadb47b6c75f028c775f192efca
rune-encoder <132966351+rune-encoder@users.noreply.github.com>
2023-10-26 17:09:55 -0500

    Added new screenshots, project information, improved effects and styling

--- src/content/portfolioContent.js
+++ src/content/portfolioContent.js
@@ -1,72 +1,123 @@
-import unityBiz from "../assets/project-photos/project-unitybiz.png"
-import techBlog from "../assets/project-photos/project-tech-blog.png"
-import noteLogger from "../assets/project-photos/project-note-logger.png"
-import vroomu from "../assets/project-photos/project-vroomu.png"
-import weatherDashboard from "../assets/project-photos/project-weather-dashboard.png"
-import workDay from "../assets/project-photos/project-work-day.png"
-import codeQuiz from "../assets/project-photos/project-code-quiz.png"
-import passwordGenerator from "../assets/project-photos/project-password-generator.png"
+import textEditor from "../assets/project-photos/project-text-editor.png";
+import socialAPI from "../assets/project-photos/project-social-network-api.png";
+import unityBiz from "../assets/project-photos/project-unitybiz.png";
+import techBlog from "../assets/project-photos/project-tech-blog.png";
+import eCommerce from "../assets/project-photos/project-ecommerce.png";
+import employeeCMS from "../assets/project-photos/project-employee-cms.png";
+import svgGenerator from "../assets/project-photos/project-svg-logo-generator.png";
+import noteLogger from "../assets/project-photos/project-note-logger.png";
+import readmeGenerator from "../assets/project-photos/peoject-readme-generator.png";
+import vroomu from "../assets/project-photos/project-vroomu.png";
+import weatherDashboard from "../assets/project-photos/project-weather-dashboard.png";
+import workDay from "../assets/project-photos/project-work-day.png";
+import codeQuiz from "../assets/project-photos/project-code-quiz.png";
+import passwordGenerator from "../assets/project-photos/project-password-generator.png";
 
 const portfolioContent = {
   title: "Portfolio",
   projects: [
     {
-      id: 1,
+      id: 14,
+      title: "Ultimate Text Editor",
+      imageUrl: `${textEditor}`,
+      deployedLink: "https://ultimate-text-editor-8e7837a44f32.herokuapp.com/",
+      githubRepo: "https://github.com/rune-encoder/Ultimate-Text-Editor",
+    },
+    {
+      id: 13,
+      title: "Social Network API",
+      imageUrl: `${socialAPI}`,
+      deployedLink: "https://www.youtube.com/watch?v=cfSfe3_7WBc",
+      githubRepo: "https://github.com/rune-encoder/Social-Network-API",
+    },
+    {
+      id: 12,
       title: "Unity Biz",
       imageUrl: `${unityBiz}`,
       deployedLink: "https://unitybiz-fd2efb602da8.herokuapp.com/",
-      githubRepo: "https://github.com/rune-encoder/UnityBiz"
+      githubRepo: "https://github.com/rune-encoder/UnityBiz",
     },
     {
-      id: 2,
+      id: 11,
       title: "Ultimate Tech Blog",
       imageUrl: `${techBlog}`,
       deployedLink: "https://u-tech-blog-84c6469a8f10.herokuapp.com/",
-      githubRepo: "https://github.com/rune-encoder/Ultimate-Tech-Blog"
+      githubRepo: "https://github.com/rune-encoder/Ultimate-Tech-Blog",
     },
     {
-      id: 3,
-      title: "Note Logger",
-      imageUrl: `${noteLogger}`,
-      deployedLink: "https://note-logger-03a612b15e73.herokuapp.com/",
-      githubRepo: "https://github.com/rune-encoder/Note-Logger"
+      id: 10,
+      title: "E-Commerce Backend",
+      imageUrl: `${eCommerce}`,
+      deployedLink: "https://www.youtube.com/watch?v=UUwZrEMcCjw",
+      githubRepo:
+        "https://github.com/rune-encoder/E-Commerce-Web-Application-BACK-END",
     },
     {
-      id: 4,
-      title: "Vroomu: A Vehicle Service Guide",
-      imageUrl: `${vroomu}`,
-      deployedLink: "https://gera1313.github.io/Vroomu-A-Vehicle-Service-Guide/",
-      githubRepo: "https://github.com/rune-encoder/Vroomu-A-Vehicle-Service-Guide"
-    },
-    {
-      id: 5,
-      title: "Weather Dashboard",
-      imageUrl: `${weatherDashboard}`,
-      deployedLink: "https://rune-encoder.github.io/Weather-Dashboard/",
-      githubRepo: "https://github.com/rune-encoder/Weather-Dashboard"
-    },
-    {
-      id: 6,
-      title: "Work Day Scheduler",
-      imageUrl: `${workDay}`,
-      deployedLink: "https://rune-encoder.github.io/Work-Day-Scheduler/",
-      githubRepo: "https://github.com/rune-encoder/Work-Day-Scheduler"
-    },
-    {
-      id: 7,
-      title: "Code Quiz",
-      imageUrl: `${codeQuiz}`,
-      deployedLink: "https://rune-encoder.github.io/Code-Quiz/",
-      githubRepo: "https://github.com/rune-encoder/Code-Quiz"
+      id: 9,
+      title: "Employee Tracker CMS",
+      imageUrl: `${employeeCMS}`,
+      deployedLink: "https://www.youtube.com/watch?v=MKeaD6Jyke8",
+      githubRepo: "https://github.com/rune-encoder/Employee-Tracker-CMS",
     },
     {
       id: 8,
+      title: "SVG Logo Generator",
+      imageUrl: `${svgGenerator}`,
+      deployedLink: "https://www.youtube.com/watch?v=b4VhrR389VU",
+      githubRepo: "https://github.com/rune-encoder/SVG-Logo-Generator",
+    },
+    {
+      id: 7,
+      title: "Note Logger",
+      imageUrl: `${noteLogger}`,
+      deployedLink: "https://ultimate-text-editor-8e7837a44f32.herokuapp.com/",
+      githubRepo: "https://github.com/rune-encoder/Note-Logger",
+    },
+    {
+      id: 6,
+      title: "README.md Generator",
+      imageUrl: `${readmeGenerator}`,
+      deployedLink: "https://www.youtube.com/watch?v=LupXOfxSzbc",
+      githubRepo: "https://github.com/rune-encoder/README.md-Generator",
+    },
+    {
+      id: 5,
+      title: "Vroomu: A Vehicle Service Guide",
+      imageUrl: `${vroomu}`,
+      deployedLink:
+        "https://gera1313.github.io/Vroomu-A-Vehicle-Service-Guide/",
+      githubRepo:
+        "https://github.com/rune-encoder/Vroomu-A-Vehicle-Service-Guide",
+    },
+    {
+      id: 4,
+      title: "Weather Dashboard",
+      imageUrl: `${weatherDashboard}`,
+      deployedLink: "https://rune-encoder.github.io/Weather-Dashboard/",
+      githubRepo: "https://github.com/rune-encoder/Weather-Dashboard",
+    },
+    {
+      id: 3,
+      title: "Work Day Scheduler",
+      imageUrl: `${workDay}`,
+      deployedLink: "https://rune-encoder.github.io/Work-Day-Scheduler/",
+      githubRepo: "https://github.com/rune-encoder/Work-Day-Scheduler",
+    },
+    {
+      id: 2,
+      title: "Code Quiz",
+      imageUrl: `${codeQuiz}`,
+      deployedLink: "https://rune-encoder.github.io/Code-Quiz/",
+      githubRepo: "https://github.com/rune-encoder/Code-Quiz",
+    },
+    {
+      id: 1,
       title: "Password Generator",
       imageUrl: `${passwordGenerator}`,
       deployedLink: "https://rune-encoder.github.io/Password-Generator/",
-      githubRepo: "https://github.com/rune-encoder/Password-Generator"
-    }
-  ]
-}
+      githubRepo: "https://github.com/rune-encoder/Password-Generator",
+    },
+  ],
+};
 
 export default portfolioContent;
--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,6 @@ import About from "./pages/About.jsx";
 import Portfolio from "./pages/Portfolio.jsx";
 import Contact from "./pages/Contact.jsx";
 import Resume from "./pages/Resume.jsx";
-// import Error from "./Error.jsx";
 
 import 'tailwindcss/tailwind.css';
 import "./styles/index.css";
