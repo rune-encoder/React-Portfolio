00303c1829c2409fd2fecd6b7c9c457814da2813
rune-encoder <132966351+rune-encoder@users.noreply.github.com>
2023-10-27 03:25:46 -0500

    Improved Dark Mode and Light Mode to set depending on the users setting in their Operating System

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,17 @@ import Main from "./components/UI/MainContent.jsx";
 import Footer from "./components/Footer.jsx";
 
 function App() {
-  const [darkMode, setDarkMode] = useState(true);
+  // Check if user prefers dark mode in their Operating System and set the theme accordingly.
+  const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)");
+  const [darkMode, setDarkMode] = useState(prefersDarkMode.matches);
 
   function toggleDarkMode() {
     setDarkMode(!darkMode);
   }
 
+  // Listen for changes in the theme if user clicked the button.
   useEffect(() => {
     const rootElement = document.getElementById("root");
-
     if (darkMode) {
       rootElement.classList.add("dark");
       rootElement.classList.remove("light");
@@ -25,6 +27,19 @@ function App() {
       rootElement.classList.remove("dark");
     }
   }, [darkMode]);
+
+  // Listen for changes if user changed theme in their Operating System.
+  // This will also apply the theme on this website.
+  useEffect(() => {
+    const mediaQueryList = window.matchMedia("(prefers-color-scheme: dark)");
+    const handleChange = (e) => {
+      setDarkMode(e.matches);
+    };
+    mediaQueryList.addEventListener("change", handleChange);
+    return () => {
+      mediaQueryList.removeEventListener("change", handleChange);
+    };
+  }, [setDarkMode]);
 
   return (
     <>
